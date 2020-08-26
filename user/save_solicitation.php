4c2066da1109b96991abceb2f842e0f0fcfcd638
GustavoRossi <gustavodevrr@gmail.com>
2020-08-26 13:30:24 -0300

    Feature add
    Implementado regra de negócio para não permitir solicitações que passem do limite

--- user/save_solicitation.php
+++ user/save_solicitation.php
@@ -11,10 +11,38 @@ header("content-type: text/html; charset=utf-8");
 $user_id = $_SESSION["logged_user_id"];
 $solicitation = json_decode($_POST["solicitation"], true);
 
+
+$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
+// Verify solicitation total 
+$selectedKitsIds = "(";
+foreach ($solicitation as $key => $value) { $selectedKitsIds = $selectedKitsIds . $value["id"] . ","; }
+$selectedKitsIds = rtrim($selectedKitsIds, ',');
+$selectedKitsIds = $selectedKitsIds . ")";
+
+$sql = "SELECT k.id, k.unitary_value FROM kit as k where k.id in $selectedKitsIds";
+$result = $conn->query($sql) or returnError("Erro ao verificar solicitação: $conn->error");
+$valuesTable = $result->fetch_all(MYSQLI_ASSOC);
+
+$sql = "SELECT u.available_founds FROM user as u where u.id = " . $_SESSION["logged_user_id"];
+$result = $conn->query($sql) or returnError("Erro ao verificar solicitação: $conn->error");
+$available_founds = ($result->fetch_object())->available_founds;
+
+$conn->commit();
+
+$total = 0;
+foreach ($solicitation as $key => $value) 
+{
+	$unitary_value = current(array_filter($valuesTable, 
+		function($element) use ($value) { return $element["id"] == $value["id"]; }))["unitary_value"];
+	$total += $unitary_value * $value["quantity"];
+}
+
+if ($total > $available_founds) { returnError("Solicitação ultrapassa limite de recursos"); }
+
 $conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
 
 $sql = "DELETE FROM solicitation as s WHERE s.user_id = '$user_id'";
-$result = $conn->query($sql) or returnError("Erro ao salvar solicitação, por favor contate o suporte: $conn->error");
+$result = $conn->query($sql) or returnError("Erro ao salvar solicitação");
 
 if (count($solicitation) <= 0) {
 	$conn->commit();
@@ -29,7 +57,7 @@ foreach ($solicitation as $key => $value) {
 $insert_values = rtrim($insert_values, ',');
 
 $sql = "INSERT INTO solicitation VALUES " . $insert_values;
-$result = $conn->query($sql) or returnError("Erro ao salvar solicitação, por favor contate o suporte: $conn->error");
+$result = $conn->query($sql) or returnError("Erro ao salvar solicitação");
 
 $conn->commit();
 $conn->close();
